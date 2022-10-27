5cbdc30ef6241ba73bb80c78704bc305c34a6932
AmarjithTK <amarjithraveendran@gmail.com>
2022-10-27 13:45:54 +0530

    some basic keybinding tweaks, before floatrules

---
+++ autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sxhkd &
+redshifter 2000 .7 &
+nitrogen --restore &
+flameshot &
+nm-applet &
+volctl &
+slstatus &
+kdeconnect-cli -l&
+sleep 10
--- config.def.h
+++ config.def.h
@@ -1,26 +1,31 @@
 /* See LICENSE file for copyright and license details. */
 
-/* appearance */
+
+#include <X11/XF86keysym.h>
+
+/* -------------------------- Endeavour dwm 2 ------------------------------- */
+
+
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayspacing = 4;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+static const char *fonts[] = { "Fira Code Medium:size=12" };
+static const char dmenufont[] = "Fira Code Medium:size=12";
+static const char col_gray1[] = "#222222";
+static const char col_gray2[] = "#425F57";
+static const char col_gray3[] = "#bbbbbb";
+static const char col_gray4[] = "#eeeeee";
+static const char col_cyan[] = "#749F82";
+static const char *colors[][3] = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = {col_gray3, col_gray1, col_gray2},
+	[SchemeSel] = {col_gray4, col_cyan, col_cyan},
 };
 
 typedef struct {
@@ -29,11 +34,9 @@ typedef struct {
 } Sp;
 
 
-const char *spcmd1[] ={ "alacritty", "-t", "spranger", "--class", "spranger", "-e", "ranger",
-	NULL };
+const char *spcmd1[] ={ "alacritty", "-t", "spranger", "--class", "spranger", "-e", "ranger",NULL };
 const char *spcmd2[] ={ "alacritty", "-t", "spmusic", "--class", "spmusic", "-e", "cmus", NULL };
-const char *spcmd3[] ={ "alacritty", "-t", "spterminal", "--config-file",
-	"/home/solan/.config/alacritty/salacritty.yml", "--class", "spterminal", NULL };
+const char *spcmd3[] ={ "alacritty", "-t", "spterminal", "--config-file","/home/solan/.config/alacritty/salacritty.yml", "--class", "spterminal", NULL };
 
 
 static Sp scratchpads[] = {
@@ -44,7 +47,7 @@ static Sp scratchpads[] = {
 };
 
 static const char *const autostart[] = {
-	"st", NULL,
+	"sh","-c","~/dwm/autostart.sh", NULL,
 	NULL /* terminate */
 };
 
@@ -58,6 +61,10 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",	  NULL,			NULL,		0,				1,			 -1 },
 	{ "Firefox",  NULL,			NULL,		1 << 8,			0,			 -1 },
+	{"kdeconnect.app", NULL, NULL, 0, 1, -1},
+	{"Galculator", NULL, NULL, 0, 1, -1},
+	{"Thunar", NULL, NULL, 0, 1, -1},
+	{"mpv", NULL, NULL, 0, 1, -1},
 	{ NULL,		  "spranger",		NULL,		SPTAG(0),		1,			 -1 },
 	{ NULL,		  "spmusic",		NULL,		SPTAG(1),		1,			 -1 },
 	{ NULL,		  "spterminal",	NULL,		SPTAG(2),		1,			 -1 },
@@ -89,11 +96,21 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
+/*------------------------------ commands--------------------------- */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+
+static const char *volupcmd[] ={ "/bin/bash", "-c", "amixer -D default sset Master Playback 5%+" };
+static const char *volmaxcmd[] ={ "/bin/bash", "-c", "amixer -D default sset Master Playback 100%" };
+static const char *voldowncmd[] ={ "/bin/bash", "-c", "amixer -D default sset Master Playback 5%-" };
+static const char *volmutecmd[] ={ "/bin/bash", "-c", "amixer -D default sset Master Playback 0%" };
+
 
 static const Key keys[] = {
+
+
+    // Basic keys
+
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
@@ -120,9 +137,18 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 
 
-	{ MODKEY,            			XK_y,  	   togglescratch,  {.ui = 0 } },
-	{ MODKEY,            			XK_u,	   togglescratch,  {.ui = 1 } },
-	{ MODKEY,            			XK_x,	   togglescratch,  {.ui = 2 } },
+    // scratch pad keyword
+	{ MODKEY|ShiftMask,            			XK_a,  	   togglescratch,  {.ui = 0 } },
+	{ MODKEY|ShiftMask,            			XK_s,  	   togglescratch,  {.ui = 1 } },
+	{ MODKEY|ShiftMask,            			XK_d,  	   togglescratch,  {.ui = 2 } },
+
+
+    // volume control keybinds
+
+	{0, XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd}},
+	{0, XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd}},
+	{MODKEY, XK_Up, spawn, {.v = volmaxcmd}},
+	{0, XF86XK_AudioMute, spawn, {.v = volmutecmd}},
 
 
 	TAGKEYS(                        XK_1,                      0)
